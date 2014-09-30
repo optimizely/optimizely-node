33a34556ca498d3279507b8b4752dfb0336206ec
Cheston Lee <cheston@optimizely.com>
2014-09-29 18:19:29 -0700

    adding initial api tests

---
+++ __tests__/apiTests.js
@@ -0,0 +1,21 @@
+'use strict';
+// jest.dontMock('../lib/services/api_factory.js');
+// jest.dontMock('request-promise');
+//
+//TODO: AutoMock does not work with request-promise
+jest.autoMockOff();
+
+describe('API Layer test suite', function() {
+  it('tests API Object creation and config definition', function() {
+    var Api = require('../lib/services/api_factory');
+    // var request = require('request-promise');
+
+    var api = Api.create({});
+    expect(api).toBeDefined();
+    expect(api._stack).toEqual([]);
+    expect(api._filter).toEqual([]);
+
+    var config = api._config;
+    expect(config).toEqual({headers: {}, transforms: {}});
+  });
+});
--- __tests__/optimizelyTest.js
+++ __tests__/optimizelyTest.js
@@ -4,19 +4,30 @@ jest.autoMockOff();
 
 describe('optimizely', function() {
   var utils = require('./testUtils');
-  var optimizely = require('../lib/optimizely')(utils.getUserOptimizelyKey());
 
-  it('creates an optimizley object with key', function() {
-    expect(optimizely).toBeDefined();
+  pit('creates an optimizley object with key', function() {
+    require('../lib/optimizely')(utils.getUserOptimizelyKey()).then(function(optly) {
+      expect(optly).toBeDefined();
+      expect(optly._api._config.headers['Token']).toBe(utils.getUserOptimizelyKey());
+    });
+  });
+
+  pit('create Optimizely instance and test for model existence', function() {
+    require('../lib/optimizely')(utils.getUserOptimizelyKey()).then(function(optly) {
+      var resources = ['projects', 'goals', 'variations', 'audiences', 'experiments'];
+
+      resources.forEach(function(resource) {
+        expect(typeof optly[resource]).toBe('object');
+        expect(typeof optly[resource].instance).toBe('function');
+      });
+    });
   });
 
   pit('getClientUserAgent returns legit UA', function() {
-    return optimizely.getClientUserAgent().then(function(ua) {
-      var uaObj = JSON.parse(ua);
-      console.log(uaObj.uname);
-      expect(uaObj).toBeDefined();
+    require('../lib/optimizely')(utils.getUserOptimizelyKey()).then(function(optly) {
+      var uaObj = JSON.parse(optly._api._config.headers['User-Agent']);
+      expect(typeof uaObj).toBe('object');
       expect(uaObj.uname).toBeDefined();
-      // expect(ua).toMatch(//);
     }, function(err) {
       expect(err).toBeFalsy();
     });
